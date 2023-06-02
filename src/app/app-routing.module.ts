51c863bd34d7e5bb2b74eb5a188a7d43ddaf6069
cher212 <e0968158@u.nus.edu>
2023-06-02 16:37:53 +0800

    first version

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'login-page',
+    loadChildren: () => import('./login-page/login-page.module').then( m => m.LoginPagePageModule)
+  },
+  {
+    path: 'forget-password',
+    loadChildren: () => import('./forget-password/forget-password.module').then( m => m.ForgetPasswordPageModule)
+  }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,35 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false
+};
+
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// export const environment = {
+//   production: false,
+//   firebaseConfig : {
+//     apiKey: "AIzaSyC0eL1AtdgHESjH8QhweLtfYgLZZcONSRs",
+//     authDomain: "silverfox-c7491.firebaseapp.com",
+//     projectId: "silverfox-c7491",
+//     storageBucket: "silverfox-c7491.appspot.com",
+//     messagingSenderId: "205889353966",
+//     appId: "1:205889353966:web:286ff946cf9381fdc69bff",
+//     measurementId: "G-47RHWRMR0P"
+//   }
+// };
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
