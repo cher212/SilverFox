dfb6345f1e66191596d8ef98851231b6caf5dfb4
Klowhe <108968734+Klowhe@users.noreply.github.com>
2023-06-10 13:49:35 +0800

    added in firebase to the project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,35 +1,32 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 export const environment = {
-  production: false
+  production: false,
+
+
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+ firebaseConfig : {
+  apiKey: "AIzaSyC0eL1AtdgHESjH8QhweLtfYgLZZcONSRs",
+  authDomain: "silverfox-c7491.firebaseapp.com",
+  databaseURL: "https://silverfox-c7491-default-rtdb.firebaseio.com",
+  projectId: "silverfox-c7491",
+  storageBucket: "silverfox-c7491.appspot.com",
+  messagingSenderId: "205889353966",
+  appId: "1:205889353966:web:286ff946cf9381fdc69bff",
+  measurementId: "G-47RHWRMR0P"
+}
 };
 
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-
-// export const environment = {
-//   production: false,
-//   firebaseConfig : {
-//     apiKey: "AIzaSyC0eL1AtdgHESjH8QhweLtfYgLZZcONSRs",
-//     authDomain: "silverfox-c7491.firebaseapp.com",
-//     projectId: "silverfox-c7491",
-//     storageBucket: "silverfox-c7491.appspot.com",
-//     messagingSenderId: "205889353966",
-//     appId: "1:205889353966:web:286ff946cf9381fdc69bff",
-//     measurementId: "G-47RHWRMR0P"
-//   }
-// };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
