49e8e887ae5859e7b88d97ee0c90a74589f2eb45
Klowhe <108968734+Klowhe@users.noreply.github.com>
2023-06-11 20:54:06 +0800

    login and validator implemented

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from 'firebase/auth';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class AuthService {
+
+  auth = getAuth();
+
+	constructor(private fauth: AngularFireAuth, private router: Router) {}
+
+	// async register({ email, password }) {
+	// 	try {
+	// 		const user = await createUserWithEmailAndPassword(this.auth, email, password);
+	// 		return user;
+	// 	} catch (e) {
+	// 		return null;
+	// 	}
+	// }
+
+  login(email: string, password: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      signInWithEmailAndPassword(this.auth, email, password)
+        .then(userCredential => {
+          const user = userCredential.user;
+          resolve(user);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  }
+
+	logout() {
+		return signOut(this.auth);
+	}
+}
