85d37ef7bf1ab078108bfde79141670ba12b47b1
cher212 <e0968158@u.nus.edu>
2023-07-01 23:39:49 +0800

    Creating apk

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+  apiKey: "AIzaSyC0eL1AtdgHESjH8QhweLtfYgLZZcONSRs",
+  authDomain: "silverfox-c7491.firebaseapp.com",
+  databaseURL: "https://silverfox-c7491-default-rtdb.firebaseio.com",
+  projectId: "silverfox-c7491",
+  storageBucket: "silverfox-c7491.appspot.com",
+  messagingSenderId: "205889353966",
+  appId: "1:205889353966:web:286ff946cf9381fdc69bff",
+  measurementId: "G-47RHWRMR0P"
+}
 };
