c4d1e7afdb24763a21a8dd69fd139f89d1009417
cher212 <e0968158@u.nus.edu>
2023-07-13 17:01:13 +0800

    elder's  alert page

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController, AlertController, LoadingController } from '@ionic/angular';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+
 
 @Component({
   selector: 'app-tab4',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Tab4Page implements OnInit {
 
-  constructor() { }
+  constructor(
+    private nav: NavController,
+    private firestore: AngularFirestore,
+  ) { }
 
   ngOnInit() {
+  }
+
+  async submit() {
+    const SWuserID = sessionStorage.getItem('swID');
+    const currentUserID = sessionStorage.getItem('userID');
+
+    if (currentUserID && SWuserID) {
+      const inChargeDocRef = this.firestore
+        .collection('profiles')
+        .doc(SWuserID)
+        .collection('in-charge')
+        .doc(currentUserID);
+
+      inChargeDocRef.update({ alerted: true })
+        .then(() => {
+          this.firestore.doc(`profiles/${currentUserID}`).update({ alerted: true })
+        })
+    }
+
+    this.nav.navigateForward(['tabs', 'tab1']);
+
   }
 
 }
